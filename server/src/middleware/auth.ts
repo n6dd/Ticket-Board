b74dfcb600974f73dad1965e1ec5c64e9e349763
Nicholas Dear <nicholasdear@aol.com>
2025-04-17 20:58:28 -0400

    changed my auth.ts in the backend again

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,30 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
+    }
+  }
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
   const authHeader = req.headers.authorization;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    // Verify the JWT token
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); 
-      }
-
-      req.user = user as JwtPayload;
-      return next(); 
-    });
-  } else {
+  if (!authHeader) {
     res.sendStatus(401); 
+    return;
   }
+
+  const token = authHeader.split(' ')[1];
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  jwt.verify(token, secretKey, (err: VerifyErrors | null, decoded: unknown) => {
+    if (err) {
+      res.sendStatus(403); 
+      return;
+    }
+
+    if (typeof decoded === 'object' && decoded && 'username' in decoded) {
+      req.user = decoded as JwtPayload;
+      next();
+    } else {
+      res.sendStatus(403); 
+    }
+  });
 };
+
 
